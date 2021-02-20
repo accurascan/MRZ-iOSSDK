168f805c734d691f559a2590822fbab69c3b11c3
Kevin <info@technzoer.com>
2021-02-20 11:04:52 +0530

    init commit

---
+++ AccuraMRZ.framework/Headers/AccuraCameraWrapper.h
@@ -0,0 +1,84 @@
+//
+//  VideoCameraWrapper.h
+//  AccuraSDK
+//
+//  Created by Chang Alex on 1/26/20.
+//  Copyright © 2020 Elite Development LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "VideoCameraWrapperDelegate.h"
+#import "SDKModels.h"
+
+#import "ResultModel.h"
+
+typedef NS_ENUM(NSUInteger, CameraFacing)
+{
+    CAMERA_FACING_BACK,
+    CAMERA_FACING_FRONT
+};
+
+typedef NS_ENUM(NSUInteger, CardSide)
+{
+    BACK_CARD_SCAN,
+    FRONT_CARD_SCAN
+};
+
+
+
+@interface AccuraCameraWrapper : NSObject
+
+{
+    CameraFacing Camerafacing;
+    CardSide ScanCard;
+    BOOL _isCapturing;
+    BOOL _isMotion;
+    NSThread *thread;
+//    SDKModel sdkm;
+}
+
+
+@property (nonatomic, strong) id<VideoCameraWrapperDelegate> delegate;
+
+
+//@property NSMutableDictionary *ocrDataSet;
+
+
+- (SDKModels *)loadEngine:(NSString *)url;
+-(void)setDefaultDialogs:(bool)isShowErrorDialogs;
+- (NSMutableArray *)getOCRList;
+
+-(id)initWithDelegate:(UIViewController<VideoCameraWrapperDelegate>*)delegate andImageView:(UIImageView *)iv andLabelMsg:(UILabel*)l andurl:(NSString*)url cardId:(int)cardId countryID:(int)countryID isScanOCR:(bool)isScanOCR andLabelMsgTop:(UILabel*)msgTop andcardName:(NSString*)cardName andcardType:(int)cardType andMRZDocType:(int)MRZDocumentType;
+-(id)initWithDelegate:(UIViewController<VideoCameraWrapperDelegate>*)delegate andImageView:(UIImageView *)iv andLabelMsg:(UILabel*)l andurl:(int)url  isBarcodeEnable:(bool)isBE countryID:(int)countryID setBarcodeType:(NSString*)setBarcodeType;
+
+-(id)initWithDelegate:(UIViewController<VideoCameraWrapperDelegate>*)delegate andImageView:(UIImageView *)iv andMsgLabel:(UILabel*)l andfeedBackframeMessage:(NSString*)feedBackframeMessage andfeedBackAwayMessage:(NSString*)feedBackAwayMessage andfeedBackOpenEyesMessage:(NSString*)feedBackOpenEyesMessage andfeedBackCloserMessage:(NSString*)feedBackCloserMessage andfeedBackCenterMessage:(NSString*)feedBackCenterMessage andfeedBackMultipleFaceMessage:(NSString*)feedBackMultipleFaceMessage andfeedBackFaceSteady:(NSString*)feedBackFaceSteady andfeedBackLowLightMessage:(NSString*)feedBackLowLightMessage andfeedBackBlurFaceMessage:(NSString*)feedBackBlurFaceMessage andfeedBackGlareFaceMessage:(NSString*)feedBackGlareFaceMessage andcheckLivess:(bool)checkLivenss;
+
+-(void)startCamera;
+-(void)stopCamera;
+-(void)ChangedOrintation:(CGFloat)width height:(CGFloat)height;
+
+-(void)processWithArray:(NSArray*)imageDataArray andarrTextData:(NSArray*)ad;
+
+-(void)processWithBack1:(NSString*)stCard  andisCheckBack:(bool)isCheckBack;
+-(void)drawFeatures:(UIImage *)image11;
+-(void)setHologramDetection:(BOOL)hologram;
+-(void)setFaceBlurPercentage:(int)faceBlur;
+-(void)setLowLightTolerance:(int)lowLight;
+-(void)setMotionThreshold:(int)motion;
+-(void)setGlarePercentage:(int)intMin intMax:(int)intMax;
+-(void)setCheckPhotoCopy:(BOOL)isPhoto;
+-(void)setLivenessGlarePercentage:(int)glareMin glareMax:(int)glareMax;
+-(void)setLivenessBlurPercentage:(int)blur;
+//-(void)setMinFrameForValidate:(int)minFrame;
+-(void)accuraSDK;
+-(void)SetCameraFacing:(CameraFacing)camera;
+-(void)SwitchCamera;
+-(void)setBlurPercentage:(int)blur;
+-(void)andCardSide:(CardSide)scanCardFirst;
+-(void)showLogFile:(bool)isShowLogs;
+-(void)CloseOCR;
+
+
+
+@end
---
+++ AccuraMRZ.framework/Headers/VideoCameraWrapperDelegate.h
@@ -0,0 +1,43 @@
+//
+//  VideoCameraWrapperDelegate.h
+//  AccuraSDK
+//
+//  Created by Chang Alex on 1/26/20.
+//  Copyright © 2020 Elite Development LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "ResultModel.h"
+
+typedef NS_ENUM(NSUInteger, RecType) {
+    REC_INIT = 1001,
+    REC_BOTH,
+    REC_FACE,
+    REC_MRZ
+};
+
+typedef NS_ENUM(NSUInteger, RecogType) {
+    OCR,
+    PDF417,
+    MRZ
+};
+
+@protocol VideoCameraWrapperDelegate <NSObject>
+@optional
+-(void)recognizeSucceedBarcode:(NSString*)message;
+-(void)processedImage:(UIImage*)image;
+-(void)recognizeFailed:(NSString*)message;
+-(void)recognizeSucceed:(NSMutableDictionary*)scanedInfo recType:(RecType)recType bRecDone:(BOOL)bRecDone bFaceReplace:(BOOL)bFaceReplace bMrzFirst:(BOOL)bMrzFirst photoImage:(UIImage*)photoImage docFrontImage:(UIImage*)docFrontImage docbackImage:(UIImage*)docbackImage;
+-(void)recognizSuccessBankCard:(NSMutableDictionary*)cardDetail andBankCardImage:(UIImage*)bankCardImage;
+
+-(void)dlPlateNumber:(NSString*)plateNumber andImageNumberPlate:(UIImage*)imageNumberPlate;
+
+-(void)resultData:(ResultModel*)resultmodel;
+-(void)reco_msg:(NSString*)message;
+-(void)livenessData:(UIImage*)livenessImage andshowImage:(UIImage*)showImage;
+-(void)screenSound;
+-(void)onUpdateLayout:(CGSize)frameSize :(float)borderRatio;
+
+
+@end
